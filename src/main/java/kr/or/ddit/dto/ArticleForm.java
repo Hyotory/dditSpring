4acc848eb61da87c5e4600e82dd92a99e8b97c4c
HYO <jerroldhj@gmail.com>
2025-05-13 09:53:51 +0900

    d

---
+++ src/main/java/kr/or/ddit/dto/ArticleForm.java
@@ -0,0 +1,16 @@
+package kr.or.ddit.dto;
+
+//자바빈 클래스 : 자바빈 규약을 지키는 클래스
+//              1) 프로퍼티 2) 기본생성자 3) getter/setter메소드
+public class ArticleForm {
+
+    // 제목 필드
+    private String title;
+
+    // 내용 필드
+    private String content;
+
+    // 기본 생성자
+    public ArticleForm() {
+    }
+}
